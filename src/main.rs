e997eb0ce9fe6a01e7e7016d282382824d82f73f
Murillo Nicacio <mnmaraes@gmail.com>
2020-12-02 19:11:24 +0000

    feat: add runner and log facilities

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,47 @@ use core::ptr;
 
 global_asm!(include_str!("start.s"));
 
-static HELLO: &[u8] = b"Hello World!";
+struct Writer;
 
-#[no_mangle]
-pub extern "C" fn run() {
-    const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+macro_rules! println {
+    () => (print!("\n"));
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
 
-    for &byte in HELLO.iter() {
-        unsafe { ptr::write_volatile(UART0, byte) }
+macro_rules! print {
+    ($($arg:tt)*) => (_print(format_args!($($arg)*)));
+}
+
+fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    Writer.write_fmt(args).unwrap();
+}
+
+impl core::fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        const UART0: *mut u8 = 0x900_0000 as *mut u8;
+
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => unsafe { ptr::write_volatile(UART0, byte) },
+                // not part of printable ASCII range
+                _ => unsafe { ptr::write_volatile(UART0, 0xfe) },
+            }
+        }
+
+        Ok(())
     }
 }
 
+#[no_mangle]
+pub extern "C" fn run() {
+    println!("Hello World!");
+}
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("Panic: {}", info);
+
     loop {}
 }
